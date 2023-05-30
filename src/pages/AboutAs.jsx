ab076a54e49420a49c948fd3f0718e897b6a0d94
Eliana <guarinoeliana@gmail.com>
2023-05-30 20:58:07 -0300

    About As

--- src/pages/AboutAs.jsx
+++ src/pages/AboutAs.jsx
@@ -5,17 +5,22 @@ function AboutAs() {
   return (
     <>
       <div className="min-h-screen flex flex-row flex-wrap justify-around items-center bg-about-as bg-center bg-cover bg-no-repeat border ">
-        <div className="w-[40%] h-[100%] border p-[3%]">
+        <div className="w-[30%] h-[100%] border p-[3%]">
           <img
-            className="h-[40%] rounded-[100%] border-8 border-quintary"
+            className="h-[30%] rounded-[100%] border-8 border-quintary"
             src="../../src/assets/photo.png"
             alt="photo"
           />
           
         </div>
-        <div className="w-[60%] h-[100%] border">
-          <h1 className="text-white text-2xl">{t("titleAbout")}</h1>
-          <p className="text-white text-2xl">{t("aboutAs")}</p>
+        <div className="w-[60%] h-[100%] pt-[3%] border">
+          <h1 className="text-white text-4xl mb-[2rem] font-bold text-center">{t("aboutAs.titleAbout")}</h1>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.1")}</p>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.2")}</p>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.3")}</p>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.4")}</p>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.5")}</p>
+          <p className="text-white m-[1rem] text-2xl">{t("aboutAs.sectionOne.6")}</p>
         </div>
       </div>
     </>
