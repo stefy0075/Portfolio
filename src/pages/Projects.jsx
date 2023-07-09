871bc443949e03d4d175a7a456744d3a98c12b31
Eliana <guarinoeliana@gmail.com>
2023-07-09 19:27:29 -0300

    Agregue Calculadora

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ import amazing from "../assets/amazing.jpg"
 import petshop from "../assets/petshop.jpg"
 import Minga from "../assets/Minga.png"
 import Odissey from "../assets/Odissey.jpg"
+import Calculadora from "../assets/calculadora.jpg"
 import './bg.css'
 
 function Proyects() {
@@ -91,7 +92,7 @@ function Proyects() {
                 alt="Petshop"
               />
               <div className="flex justify-center">
-                <a href="https://github.com/stefy0075/Minga-Web-Front" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/stefy0075/Odissey-1.2-front" target="_blank" rel="noopener noreferrer">
                   <FaGithub className="text-white text-3xl m-3 hover:text-gray-500" />
                 </a>
                 <a href="https://odisseytravel.vercel.app/" target="_blank" rel="noopener noreferrer">
@@ -101,6 +102,27 @@ function Proyects() {
             </div>
             <h2 className="text-white font-bold text-xl mt-[0.5em] text-center pb-[0.5%]">
               Odissey Travel
+            </h2>
+          </div>
+
+          <div className="transform mb-[1rem] flex flex-col justify-center items-center hover:scale-105 w-[15rem] md:w-[20rem]">
+            <div className="rounded-[1em] w-[15rem]  md:w-[20rem] css bg-primary m-[2%]">
+              <img
+                className="w-[15rem] md:h-[11rem] h-[7.5rem]  object-cover object-top md:w-[20rem] rounded-t-[1em]"
+                src={Calculadora}
+                alt="Petshop"
+              />
+              <div className="flex justify-center">
+                <a href="https://github.com/stefy0075/calculadora-basica" target="_blank" rel="noopener noreferrer">
+                  <FaGithub className="text-white text-3xl m-3 hover:text-gray-500" />
+                </a>
+                <a href="https://calculadora0075.vercel.app/" target="_blank" rel="noopener noreferrer">
+                  <FaGlobe className="text-white text-3xl m-3 hover:text-gray-500 " />
+                </a>
+              </div>
+            </div>
+            <h2 className="text-white font-bold text-xl mt-[0.5em] text-center pb-[0.5%]">
+              Calculadora
             </h2>
           </div>
           
