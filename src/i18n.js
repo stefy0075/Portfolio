392f374e68965e9516083819bb6d4f14b8a1966a
Stefani Guarino <guarinoeliana@gmail.com>
2025-07-10 16:18:24 -0300

    permanencia del idioma

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-import translationEN from './locales/en/translation.json';
-import translationES from './locales/es/translation.json';
-import translationIT from './locales/it/translation.json';
+import enTranslation from '../en.json';
+import esTranslation from '../es.json';
+import itTranslation from '../it.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: translationEN },
-      es: { translation: translationES },
-      it: { translation: translationIT },
+      en: { translation: enTranslation },
+      es: { translation: esTranslation },
+      it: { translation: itTranslation },
     },
     fallbackLng: 'en',
     detection: {
